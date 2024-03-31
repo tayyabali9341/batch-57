cdec7795dc02cf12266b9c45454529305df19281
tayyab ali <tayyabalilibra@gmail.com>
2024-03-31 15:11:22 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log("hello i am from panaverse");
+//and
+//T T T
+//F T F
+//F T F
+//OR
+//T T T
+//T F T
+//F T T
+//F F F
+//NOT !
+//T F
+//F T
+// condition
+//if else
+n = 6;
+if (n == 5) {
+    console.log("if condition");
+}
+else {
+    console.log("else part");
+}
